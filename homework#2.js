ddb71268d73a775d9a2001a7facc4d64a317396a
Luciano Salinas <35172342+Lucianosalinas38@users.noreply.github.com>
2023-05-16 10:28:49 -0500

    Update homework#2.js

--- homework#2.js
+++ homework#2.js
@@ -1,27 +1,16 @@
-// let dessert = ["pie", "Taco","chicken"]
-
-// for (i = 0; i < 10; i++){
-//     guest = {}
-//     guest.teaCup = true;
-//     guest.teaPlate = true;
-//     guest.firstName = "Luciano";
-//     guest.lastName = "Salians";
-//     guest.dessert = "Pie";
-// }
-
-// function pickRandom(array) = Math.floor(Math.random() * Array.length)
 let arr = [-5, 13, 22, -1000, 55];
 let sorted = false;
 
 while (!sorted) {
-  sorted = true;
+  //Will always run until the array is sorted
+  sorted = true; //Declared true at the start so once its sorted it will end and wont run anymore
 
   for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i] > arr[i + 1]) {
       let temp = arr[i];
       arr[i] = arr[i + 1];
       arr[i + 1] = temp;
-      sorted = false;
+      sorted = false; //Delcared false once its ran through the for loop to ensure to check again to make sure its fully sorted
     }
   }
 }
